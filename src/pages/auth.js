670dd31ee60d3f11ee3d8d865d07d5f754f8db9b
gelastropod <gelastropodyt@gmail.com>
2024-11-08 23:25:27 +0800

    no funny names please!!!

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -21,11 +21,11 @@ function Auth () {
                     <input type="password" class="input-field" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
 
                     <a class="login-btn" onClick={()=>{
-                        if (teacher_list.users[email].password === password)
+                        if (teacher_list.users[email] === password)
                         {
                             window.location.href="../Dashboard/Teachers";
                             console.log("teacher");
-                        }else if(students_list.users[email].password === password){
+                        }else if(students_list.users[email] === password){
                             window.location.href="../Dashboard/Students";
                             console.log("student");
                         }else{
