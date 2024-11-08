0ee2579d140fa306591b33b2ae9ce677ded83354
Zhehan <r1ftwalk3r62@gmail.com>
2024-11-08 17:13:59 +0800

    added home page

--- src/Titlebar.js
+++ src/Titlebar.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function Titlebar() {
     return (
-        <header>a</header>
+        <React.Fragment>
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className='creds'><button className='credbutton'>Log In</button></div>
+        </React.Fragment>
     );
 }
 
